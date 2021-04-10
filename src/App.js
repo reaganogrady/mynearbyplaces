8ff461f35536c11ceefd5c415cb8494b70262f04
Reagan <reagan@email.arizona.edu>
2021-04-10 00:42:23 -0700

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
-import { HashRouter, Switch } from "react-router-dom";
+import Home from './home';
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
+    <Container>
     <HashRouter>
       <Switch>
-        <Route exact path ="/" component= { Home } />
+        <Route path ="/home" component= { Home } />
       </Switch>
     </HashRouter>
+
+    <Navbar fixed="top" expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand> YelpJr </Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home"> Home </Nav.Link>
+          <Nav.Link href="#login"> Login </Nav.Link>
+          <Nav.Link href="#signup"> Sign Up </Nav.Link>
+        </Nav>
+    </Navbar>
+    </Container>
   );
 }
 
---
+++ src/home.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import './App.css';
+
+function Home (){
+        console.log("Home");
+        return(
+           <h1>home</h1>
+        
+        );
+    
+}
+
+export default Home;
