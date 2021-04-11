5bb65bc7910b3e68ae63393f365e05fdca014f73
Reagan <reagan@email.arizona.edu>
2021-04-10 21:34:55 -0700

    Search bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from './home';
 import Login from './login';
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { Container, Nav } from 'react-bootstrap';
+import logo from './images/logo.png';
+import {Button, Form} from 'react-bootstrap';
+import FormControl from 'react-bootstrap/FormControl'
 
 function App() {
   return (
@@ -15,14 +18,22 @@ function App() {
       </Switch>
     </HashRouter>
 
-    <Navbar fixed="top" expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand> YelpJr </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link href="#home"> Home </Nav.Link>
+    <Navbar fixed="top" expand="lg" bg="light" variant="light">
+        <Navbar.Brand>  <img src={logo} alt="logo" height="50px"/></Navbar.Brand>
+        <Nav>
+          <Nav.Link href="#home">  Home </Nav.Link>
           <Nav.Link href="#login"> Login </Nav.Link>
           <Nav.Link href="#signup"> Sign Up </Nav.Link>
         </Nav>
+        <Nav className="ml-auto">
+          <Form inline> 
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="dark">Search</Button>
+          </Form>
+        </Nav>
     </Navbar>
+
+    
     </Container>
   );
 }
--- src/login.js
+++ src/login.js
@@ -24,6 +24,7 @@ function Login() {
         </Form.Group>
         <Button type="submit">Submit</Button>
     </Form>
+    
     );
 }
 
