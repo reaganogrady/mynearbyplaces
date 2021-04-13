ac47db1a3d0458221973ed84e9ed28da27d8660c
Reagan <reagan@email.arizona.edu>
2021-04-12 17:31:28 -0700

    Search bar

--- src/home.js
+++ src/home.js
@@ -1,11 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.css';
+import {Button, Form, Col} from 'react-bootstrap';
+
 
 function Home (){
         console.log("Home");
         return(
-         <h1>home</h1>
+         <Form>
+             <Form.Row className="align-items-center">
+             <Col xs={7}>
+                <Form.Control className="my-1" id="Search" placeholder="What are you looking for?"/>
+            </Col>
+            <Col>
+                <Form.Control className="my-1" id="Location" placeholder="Near"/>
+            </Col>
+            <Col>
+                <Button variant="dark">Search</Button>
+            </Col>
+            </Form.Row>
+         </Form>
         
         );
     
