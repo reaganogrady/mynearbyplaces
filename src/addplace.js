91e642858dc6ebf21b633fa5b31653a1a9636c1a
Reagan <reagan@email.arizona.edu>
2021-04-12 18:01:16 -0700

    newest

---
+++ src/addplace.js
@@ -0,0 +1,52 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import './App.css';
+import {Button, Form} from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { Container, Col, Row } from 'react-bootstrap';
+
+function AddPlace() {
+    const history = useHistory();
+
+    let onPlaceSubmitted = () => {
+        history.push('/');
+    }
+    return(
+        <Container className="form">
+                <h4> Add a Place </h4>
+                <Form onSubmit={onPlaceSubmitted}>
+                    <Row >
+                    <Col xs={5}>
+                        <Form.Group>
+                            <Form.Label>Business Name</Form.Label> 
+                            <Form.Control placeholder="Business"/>
+                        </Form.Group>
+                    </Col>
+                    <Col xs={4} >
+                        <Form.Group>
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={10}>
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control as="select" size="sm" custom>
+                                <option>Restaurant</option>
+                                <option>Home services</option>
+                                <option>Salon</option>
+                                <option>Retail Store</option>
+                                <option>Other</option>
+                            </Form.Control>
+                        </Col>
+                    </Row>
+                    <br/>
+                    <Button type="submit">Submit</Button>
+                   
+                </Form>
+            </Container>
+    );
+}
+
+export default AddPlace;
